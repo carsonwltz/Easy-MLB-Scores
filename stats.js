01513c94e3a107a5afbcc64f36209b56d7b1e9c5
carsonwltz <carson.uvwwv@simplelogin.co>
2024-04-29 07:25:24 -0500

    Displayed away team name from API

--- stats.js
+++ stats.js
@@ -1,34 +1,23 @@
-// Place of Origin
-fetch('https://statsapi.mlb.com/api/v1/schedule/games/?sportId=1')
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("NETWORK RESPONSE ERROR");
-    }
-  })
-  // What to do with data
-  .then(data => displaySchedule(data))
-  .catch((error) => console.error("FETCH ERROR:", error));
+fetch("https://statsapi.mlb.com/api/v1/schedule/games/?sportId=1")
+	.then((response) => {
+		if (response.ok) {
+			return response.json();
+		} else {
+			throw new Error("NETWORK RESPONSE NOT OK");
+		}
+	})
+	.then(function (data) {
+		console.log(data);
+		displayData(data);
+	})
+	.catch((error) => {
+		console.error("FETCH ERROR:", error);
+	});
 
-function displaySchedule(data) {
-
-// Finds what directory
-const schedule = data.dates[0]; 
-const games = schedule.games[0];
-const teams = games.teams[0];
-const away = teams.away[0];
-const awayTeam = away.team [0];
-// Finds what item from directory
-const awayTeamName = awayTeam.name;
-// Finds ID on HTML file
-  const awayTeamDiv = document.getElementById("dates");
-// Creates header    
-  const heading = document.createElement("h1");
-// Places item in header in HTML file
-  heading.innerHTML = awayTeamName;
-// Places header in ID in HTML file
-  awayTeamDiv.appendChild(heading);
+function displayData(data) {
+	const awayTeam1 = data.dates[0].games[0].teams.away.team.name;
+	const awayTeam1Div = document.getElementById("display-data");
+	const heading = document.createElement("h1");
+	heading.innerHTML = awayTeam1;
+	awayTeam1Div.appendChild(heading);
 }
-
-
