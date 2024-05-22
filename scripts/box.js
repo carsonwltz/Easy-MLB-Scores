3ef603ed1f915e36c0de3f590cd6d26cae078ebc
carsonwltz <carson.uvwwv@simplelogin.co>
2024-05-22 15:52:48 -0500

    Commit #8 - General cleanup - Styled boxscore links (instead made the entire game div a link) - Added on hover effects for each game - Hides times when games end

--- scripts/box.js
+++ scripts/box.js
@@ -17,15 +17,55 @@ function displayData5(data) {
 	// Box #1
 	const gamePk1 = data.dates[0].games[0].gamePk;
 	const gameBoxDiv1 = document.getElementById("gameBox1");
-	if (data.dates[0].games[0].status.detailedState === "Final") {
-		gameBoxDiv1.setAttribute(
-			"href",
-			`https://www.mlb.com/gameday/${gamePk1}/final/box`
-		);
+	if (data.dates[0].games[0].status.detailedState === "Scheduled") {
+		gameBoxDiv1.setAttribute("href", `#`);
 	} else {
-		gameBoxDiv1.setAttribute(
-			"href",
-			`https://www.mlb.com/gameday/${gamePk1}/final/box`
-		);
+		if (data.dates[0].games[0].status.detailedState === "Final") {
+			gameBoxDiv1.setAttribute(
+				"href",
+				`https://www.mlb.com/gameday/${gamePk1}/final/box`
+			);
+		} else {
+			gameBoxDiv1.setAttribute(
+				"href",
+				`https://www.mlb.com/gameday/${gamePk1}`
+			);
+		}
+	}
+	// Box #2
+	const gamePk2 = data.dates[0].games[1].gamePk;
+	const gameBoxDiv2 = document.getElementById("gameBox2");
+	if (data.dates[0].games[1].status.detailedState === "Scheduled") {
+		gameBoxDiv2.setAttribute("href", `#`);
+	} else {
+		if (data.dates[0].games[1].status.detailedState === "Final") {
+			gameBoxDiv2.setAttribute(
+				"href",
+				`https://www.mlb.com/gameday/${gamePk2}/final/box`
+			);
+		} else {
+			gameBoxDiv2.setAttribute(
+				"href",
+				`https://www.mlb.com/gameday/${gamePk2}`
+			);
+		}
+	}
+	// Box #3
+	const gamePk3 = data.dates[0].games[2].gamePk;
+	const gameBoxDiv3 = document.getElementById("gameBox3");
+	if (data.dates[0].games[2].status.detailedState === "Scheduled") {
+		gameBoxDiv3.setAttribute("href", `#`);
+	} else {
+		if (data.dates[0].games[2].status.detailedState === "Final") {
+			gameBoxDiv3.setAttribute(
+				"href",
+				`https://www.mlb.com/gameday/${gamePk3}/final/box`
+			);
+		} else {
+			gameBoxDiv3.setAttribute(
+				"href",
+				`https://www.mlb.com/gameday/${gamePk3}`
+			);
+		}
 	}
 }
--- scripts/times.js
+++ scripts/times.js
@@ -15,78 +15,87 @@ fetch("https://statsapi.mlb.com/api/v1/schedule/games/?sportId=1")
 	});
 function displayData4(data) {
 	// Game Time #1
-	const gamePk1 = data.dates[0].games[0].gamePk;
-	fetch(`https://statsapi.mlb.com/api/v1.1/game/${gamePk1}/feed/live`)
-		.then((response) => {
-			if (response.ok) {
-				return response.json();
-			} else {
-				throw new Error("NETWORK RESPONSE NOT OK");
-			}
-		})
-		.then((data) => {
-			const gameTime1 = data.gameData.datetime.time;
-			const gameTime1AMPM = data.gameData.datetime.ampm;
-			const gameTime1Div = document.getElementById("gameTime1");
-			const gameTime1AMPMDiv = document.getElementById("gameTime1AMPM");
-			const heading1 = document.createElement("span");
-			const heading2 = document.createElement("span");
-			heading1.innerHTML = gameTime1;
-			gameTime1Div.appendChild(heading1);
-			heading2.innerHTML = gameTime1AMPM;
-			gameTime1AMPMDiv.appendChild(heading2);
-		})
-		.catch((error) => {
-			console.error("FETCH ERROR:", error);
-		});
+	if (data.dates[0].games[2].status.detailedState === "Final") {
+	} else {
+		const gamePk1 = data.dates[0].games[0].gamePk;
+		fetch(`https://statsapi.mlb.com/api/v1.1/game/${gamePk1}/feed/live`)
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				} else {
+					throw new Error("NETWORK RESPONSE NOT OK");
+				}
+			})
+			.then((data) => {
+				const gameTime1 = data.gameData.datetime.time;
+				const gameTime1AMPM = data.gameData.datetime.ampm;
+				const gameTime1Div = document.getElementById("gameTime1");
+				const gameTime1AMPMDiv = document.getElementById("gameTime1AMPM");
+				const heading1 = document.createElement("span");
+				const heading2 = document.createElement("span");
+				heading1.innerHTML = gameTime1;
+				gameTime1Div.appendChild(heading1);
+				heading2.innerHTML = gameTime1AMPM;
+				gameTime1AMPMDiv.appendChild(heading2);
+			})
+			.catch((error) => {
+				console.error("FETCH ERROR:", error);
+			});
+	}
 	// Game Time #2
-	const gamePk2 = data.dates[0].games[1].gamePk;
-	fetch(`https://statsapi.mlb.com/api/v1.1/game/${gamePk2}/feed/live`)
-		.then((response) => {
-			if (response.ok) {
-				return response.json();
-			} else {
-				throw new Error("NETWORK RESPONSE NOT OK");
-			}
-		})
-		.then((data) => {
-			const gameTime2 = data.gameData.datetime.time;
-			const gameTime2AMPM = data.gameData.datetime.ampm;
-			const gameTime2Div = document.getElementById("gameTime2");
-			const gameTime2AMPMDiv = document.getElementById("gameTime2AMPM");
-			const heading3 = document.createElement("span");
-			const heading4 = document.createElement("span");
-			heading3.innerHTML = gameTime2;
-			gameTime2Div.appendChild(heading3);
-			heading4.innerHTML = gameTime2AMPM;
-			gameTime2AMPMDiv.appendChild(heading4);
-		})
-		.catch((error) => {
-			console.error("FETCH ERROR:", error);
-		});
+	if (data.dates[0].games[2].status.detailedState === "Final") {
+	} else {
+		const gamePk2 = data.dates[0].games[1].gamePk;
+		fetch(`https://statsapi.mlb.com/api/v1.1/game/${gamePk2}/feed/live`)
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				} else {
+					throw new Error("NETWORK RESPONSE NOT OK");
+				}
+			})
+			.then((data) => {
+				const gameTime2 = data.gameData.datetime.time;
+				const gameTime2AMPM = data.gameData.datetime.ampm;
+				const gameTime2Div = document.getElementById("gameTime2");
+				const gameTime2AMPMDiv = document.getElementById("gameTime2AMPM");
+				const heading3 = document.createElement("span");
+				const heading4 = document.createElement("span");
+				heading3.innerHTML = gameTime2;
+				gameTime2Div.appendChild(heading3);
+				heading4.innerHTML = gameTime2AMPM;
+				gameTime2AMPMDiv.appendChild(heading4);
+			})
+			.catch((error) => {
+				console.error("FETCH ERROR:", error);
+			});
+	}
 	// Game Time #3
-	const gamePk3 = data.dates[0].games[2].gamePk;
-	fetch(`https://statsapi.mlb.com/api/v1.1/game/${gamePk3}/feed/live`)
-		.then((response) => {
-			if (response.ok) {
-				return response.json();
-			} else {
-				throw new Error("NETWORK RESPONSE NOT OK");
-			}
-		})
-		.then((data) => {
-			const gameTime3 = data.gameData.datetime.time;
-			const gameTime3AMPM = data.gameData.datetime.ampm;
-			const gameTime3Div = document.getElementById("gameTime3");
-			const gameTime3AMPMDiv = document.getElementById("gameTime3AMPM");
-			const heading5 = document.createElement("span");
-			const heading6 = document.createElement("span");
-			heading5.innerHTML = gameTime3;
-			gameTime3Div.appendChild(heading5);
-			heading6.innerHTML = gameTime3AMPM;
-			gameTime3AMPMDiv.appendChild(heading6);
-		})
-		.catch((error) => {
-			console.error("FETCH ERROR:", error);
-		});
+	if (data.dates[0].games[2].status.detailedState === "Final") {
+	} else {
+		const gamePk3 = data.dates[0].games[2].gamePk;
+		fetch(`https://statsapi.mlb.com/api/v1.1/game/${gamePk3}/feed/live`)
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				} else {
+					throw new Error("NETWORK RESPONSE NOT OK");
+				}
+			})
+			.then((data) => {
+				const gameTime3 = data.gameData.datetime.time;
+				const gameTime3AMPM = data.gameData.datetime.ampm;
+				const gameTime3Div = document.getElementById("gameTime3");
+				const gameTime3AMPMDiv = document.getElementById("gameTime3AMPM");
+				const heading5 = document.createElement("span");
+				const heading6 = document.createElement("span");
+				heading5.innerHTML = gameTime3;
+				gameTime3Div.appendChild(heading5);
+				heading6.innerHTML = gameTime3AMPM;
+				gameTime3AMPMDiv.appendChild(heading6);
+			})
+			.catch((error) => {
+				console.error("FETCH ERROR:", error);
+			});
+	}
 }
